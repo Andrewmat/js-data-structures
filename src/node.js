2a5c557c8618bfcabb77720e7af457b15553f804
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2017-01-30 14:02:36 -0200

    Add comments to specify the code

--- src/node.js
+++ src/node.js
@@ -1,11 +1,13 @@
 'use strict'
 
+// Factory function to instantiate a container of a value
 const node = (state) => {
     let getValue = () => {
         return state.value;
     };
     return { getValue };
 }
+// Factory function to instantiate an iterable that has a pointer to another iterable
 const iterable = (state) => {
     // duck-typing to check if an object is iterable
     this.is = (object) =>
@@ -22,6 +24,7 @@ const iterable = (state) => {
     return { getNext };
 }
 
+// Factory function to instantiate a node that has a link to another node
 const linkNode = (state) => {
     return Object.assign(
         {},
@@ -30,6 +33,7 @@ const linkNode = (state) => {
     );
 }
 
+// Checking the correctness of the link node
 let myNode1 = linkNode({
     value: 1,
     next: null
