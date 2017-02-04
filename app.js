d75c22045db25e825aead857b1dfaf216f72615e
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2017-02-04 12:07:55 -0200

    * Change jsHint to jslint * Add settings file for vscode debug

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 'use strict';
+
 const src = require('./src.js');
-const { linkedList, doubleLinkedList } = require(src.list);
+const { doubleLinkedList } = require(src.list);
 
-let mylst = doubleLinkedList();
 
-for (let i = 0; i < 30; ++i) {
-    mylst.add('' + i + 'var');
+function main() {
+    let mylst = doubleLinkedList();
+    for (let i = 0; i < 30; ++i) {
+        mylst.add('' + i + 'var');
+    }
+
+    console.log('length ' + mylst.length() + ':');
+    mylst.forEach((el, ind) => {
+        console.log('lst[' + ind + ']: ' + el);
+    });
 }
-
-console.log('length ' + mylst.length() + ':');
-mylst.forEach((el, ind) => {
-    console.log('lst[' + ind + ']: ' + el);
-});
+main();
--- src/iterable.js
+++ src/iterable.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const nextable = (state) => {
     this.isNextable = (object) =>
